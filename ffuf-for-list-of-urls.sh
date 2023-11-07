0a8db73324a43892b4a0142fad9882ee42b70aeb
vhssunny1 <38647441+vhssunny1@users.noreply.github.com>
2023-11-07 19:42:26 +0530

    Update ffuf-for-list-of-urls.sh

--- ffuf-for-list-of-urls.sh
+++ ffuf-for-list-of-urls.sh
@@ -1,8 +1,35 @@
-target=$1;
-wordlist=$2;
-while read -r url;
+#!/bin/bash
 
-do ffuf -w $wordlist -u $url/FUZZ -c -mc 200-299 | tee ./$(echo $url | sed 's/http:\/\//dir-/g' | sed 's/https:\/\//dir-/g' | sed 's/:/\-/g').txt;
+# Check if the required arguments are provided
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <target> <wordlist>"
+  exit 1
+fi
 
-done < $target
-~
+target="$1"
+wordlist="$2"
+
+# Function to create a safe filename from the URL
+sanitize_url() {
+  echo "$1" | sed -e 's|https\?://||' -e 's/[^A-Za-z0-9_.-]/_/g'
+}
+
+# Ensure the target and wordlist files exist
+if [ ! -f "$target" ]; then
+  echo "Target file '$target' does not exist."
+  exit 1
+fi
+
+if [ ! -f "$wordlist" ]; then
+  echo "Wordlist file '$wordlist' does not exist."
+  exit 1
+fi
+
+# Loop through the URLs in the target file
+while read -r url; do
+  sanitized_url=$(sanitize_url "$url")
+  output_file="dir-${sanitized_url}.txt"
+
+  # Use ffuf with improved options
+  ffuf -w "$wordlist" -u "$url/FUZZ" -c -mc 200-299 | tee "./$output_file"
+done < "$target"
